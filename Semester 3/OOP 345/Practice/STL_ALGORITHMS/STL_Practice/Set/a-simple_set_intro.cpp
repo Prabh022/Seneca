67a30fe50d72279f1e427b0283f2d54da8643fff
Prabhjeet Singh <singhprabh23064@gmail.com>
2025-07-24 09:59:08 -0400

    Worked on some concepts of Set of STL.

---
+++ Semester 3/OOP 345/Practice/STL_ALGORITHMS/STL_Practice/Set/a-simple_set_intro.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <set>
+
+int main() {
+    std::set<int> s;
+
+    s.insert(5);
+    s.insert(8);
+    s.insert(5);
+    s.insert(34);
+
+    std::cout << "Set Contents: ";
+    for (int x : s) {
+        std::cout << x << " ";
+    }
+    return 0;
+}
