0334b72428a8d5e1d3ad004e7e37845333064e1c
Prabhjeet Singh <singhprabh23064@gmail.com>
2025-07-24 19:58:04 -0400

    some general concept as rule of 3, and rule of 5.

---
+++ Semester 3/OOP 345/Practice/General/e-ruleof5_example.cpp
@@ -0,0 +1,73 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Book {
+    char* title;
+
+public:
+    Book(const char* name) {
+        // allocate memory for title
+        cout << "Constructor" << endl;
+        title = new char[strlen(name) + 1];
+        strcpy(title, name);
+   }
+    Book(const Book& other) {
+        // allocate new memory and copy title
+        cout << "Copy Constructor" << endl;
+        title = new char[strlen(other.title) + 1];
+        strcpy(title, other.title);
+    }
+
+    // copy assignment constructor
+    Book& operator=(const Book& other) {
+        cout << "Copy Assignment Constructor" << endl;
+        if (this != &other) {
+            delete[] title;
+
+            title = new char[strlen(other.title) + 1];
+            strcpy(title, other.title);
+        }
+        return *this;
+    }
+    // Move Constructor
+    Book(Book&& other) noexcept {
+        cout << "Move Constructor" << endl;
+        title = other.title;
+        title = nullptr;
+    }
+    // Move Assignment Constructor
+    Book& operator=(Book&& other) noexcept {
+        if (this != &other) {
+            delete[] title;
+
+            cout << "Move Assignment Constructor" << endl;
+            title = other.title;
+            title = nullptr;
+        }
+        return *this;
+    }
+    // Destructor
+    ~Book() {
+        cout << "Destructor" << endl;
+        delete[] title;
+    }
+    void show() const {
+        cout << "Name of the book is: " << title << endl;
+    }
+};
+
+int main() {
+    Book b1("C++ Mastery");
+    Book b2 = b1;
+    Book b3("Temporary");
+    b3 = b2;
+    b1.show();
+    b2.show();
+    b3.show();
+    Book b4 = std::move(b1);   // Move Constructor
+    Book b5("Learning C++");
+    b5 = std::move(b2);        // Move Assignment
+}
