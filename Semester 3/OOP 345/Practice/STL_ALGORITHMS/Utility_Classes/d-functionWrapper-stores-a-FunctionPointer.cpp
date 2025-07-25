6f12f9f14c8a11e96761533f60596d7d71c2d58c
Prabhjeet Singh <singhprabh23064@gmail.com>
2025-07-25 19:32:27 -0400

    I have included some files where I have used function wrapper to store or manage functors, lambda's and a regular function or a pointer to a function.

---
+++ Semester 3/OOP 345/Practice/STL_ALGORITHMS/Utility_Classes/d-functionWrapper-stores-a-FunctionPointer.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<functional>
+
+int square(int x) {
+	return x * x;
+}
+
+
+using namespace std;
+int main() {
+	std::function<int(int)> f1 = square;
+	std::cout << f1(5); 
+
+	return 0;
+}
