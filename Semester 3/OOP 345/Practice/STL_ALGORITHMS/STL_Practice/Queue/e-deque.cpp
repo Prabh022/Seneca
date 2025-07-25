097727e44aeb787b6b8a9ea3977b07d755e9043d
Prabhjeet Singh <singhprabh23064@gmail.com>
2025-07-25 10:44:15 -0400

    was having some issues with commit, was not properly synced for some reasons. Trying again.

---
+++ Semester 3/OOP 345/Practice/STL_ALGORITHMS/STL_Practice/Queue/e-deque.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<queue>
+
+int main() {
+
+	std::deque<int> nums = { 10, 20, 30 };
+	nums.push_front(5);
+	nums.push_back(40);
+	std::cout << "Numbers stored are: ";
+	for (int num : nums) {
+		std::cout << num << " ";
+	}
+	std::cout << "\nElement at index 2: " << nums[2];
+
+
+	return 0;
+}
