f3d9b0ae88c792e866a285c9d1cc4538d3072abc
Prabhjeet Singh <singhprabh23064@gmail.com>
2025-08-17 13:58:10 -0400

    some changes made

--- Semester 3/WEB322/Practice/MyTailwindServer/tailwind.config.js
+++ Semester 3/WEB322/Practice/MyTailwindServer/tailwind.config.js
@@ -10,4 +10,8 @@ module.exports = {
   content: [`./views/**/*.html`], // all .html files
   // ...
 };
+module.exports = {
+  // ...
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+};
 
